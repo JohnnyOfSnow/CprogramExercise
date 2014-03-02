cbb7d0f1eea908af7c72fc010f6704125e9ceae1
JavaExercise征戰 <good258johnny@yahoo.com.tw>
2014-03-02 17:20:21 +0800

    Create evaluateE.cpp
    完成141頁第3.45的第2題

---
+++ evaluateE.cpp
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+
+int main(void){
+    int ziNumber;
+    printf("You can enter a non-negative number to estimate the natural number e\n");
+    printf("The number is: ");
+    scanf("%d", ziNumber);
+    
+    // The following is counter-control to estimate the natural number e
+		int counter = 1;
+		double x, sum;
+		x = 1.0;
+		sum = 1.0;
+		while(counter <= ziNumber) {
+			x = x / counter;
+			sum = sum + x;
+			counter = counter + 1;
+		}
+    
+    printf("The result of estimating is %.6f", sum);
+}
