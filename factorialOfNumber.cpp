9221148f589b296e2ae21f8904cfa4328bc305a1
JavaExercise征戰 <good258johnny@yahoo.com.tw>
2014-03-03 16:10:49 +0800

    Update factorialOfNumber.cpp
    Debug

--- factorialOfNumber.cpp
+++ factorialOfNumber.cpp
@@ -4,16 +4,15 @@ int main(void){
     int ziNumber;
     printf("You can enter a non-negative number to give a factorial of the number\n");
     printf("The number is: ");
-    scanf("%d", ziNumber);
+    scanf("%d", &ziNumber);
     
     int result = 1;
     int counter = 0;
-		int zinumber = number;
-		counter = zinumber;
-		while(counter > 0){
-			result = result * counter;
-			counter = counter - 1;
-		}
+    counter = zinumber;
+    while(counter > 0){
+	result = result * counter;
+	counter = counter - 1;
+    }
 		
-		printf("%d! is %d",ziNumber, result);
+    printf("%d! is %d",ziNumber, result);
 }
