a50695cf42280083415eda2e3adf932994f02163
JavaExercise征戰 <good258johnny@yahoo.com.tw>
2014-03-03 18:04:10 +0800

    Create piEstimate.cpp
    今天的作業 3/3

---
+++ piEstimate.cpp
@@ -0,0 +1,21 @@
+int main(void){
+	printf("%s  %9s","pi","estimate pi\n" );
+	double pi;
+	double number;
+	double reNumber;
+	reNumber = -1.0;
+	pi = 0.0;
+	number = 1.0;
+	for(int i = 1; i <= 1; i++){
+		reNumber = reNumber + 2;
+		number = number / reNumber;
+		if(i % 2 == 0){
+			number = -1 * number;
+		}
+		pi = pi + (4 * number);
+		number = 1.0;
+		printf("%2d  %.6f\n",i,pi );
+		
+	}
+
+}
