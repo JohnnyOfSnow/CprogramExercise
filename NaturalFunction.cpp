5a27109732b52b5b77ed88b805b571b623ae3b3e
JavaExercise征戰 <good258johnny@yahoo.com.tw>
2014-03-03 17:04:41 +0800

    Debug
    誤會題目意思，題目需要提供2種值(1個是階層的，1個是變數x)

--- NaturalFunction.cpp
+++ NaturalFunction.cpp
@@ -2,9 +2,13 @@
 
 int main(void){
     int ziNumber;
+    int x;
     printf("You can enter a non-negative number to estimate the power of natural number e\n");
+    printf("You should enter a integer to decide how many times you want to do\n");
     printf("The number is: ");
-    scanf("%d", ziNumber);
+    scanf("%d", &ziNumber);
+    printf("And, you should decide the variable x is: ");
+    scanf("%d", &x);
     
     // The following is counter-control to estimate the power of natural number e
     int ziNumber;
@@ -24,7 +28,7 @@ int main(void){
 			printf("The %d! is %f\n", counter,sectionOneResult);
 			
 			while(sectionTwoCounter <= counter) {
-				sectionTwoResult = sectionTwoResult * ziNumber;				
+				sectionTwoResult = sectionTwoResult * x;				
 				sectionTwoCounter = sectionTwoCounter + 1;
 			}
 			printf("The %d is %d\n", counter,sectionTwoResult);
