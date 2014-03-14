a37da47fba9ead9ef932bfd1ae8d86355d431a25
ProgramExercise征戰 <good258johnny@yahoo.com.tw>
2014-03-14 20:44:08 +0800

    Debug and Test
    第2函式與第3函式以指標提供位置

--- crapsFixByFourSub.cpp
+++ crapsFixByFourSub.cpp
@@ -2,37 +2,28 @@
 // Simulating the game of craps.
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h> 
+#include <time.h>
 
 
 int rollDice( void ); // function prototype
 int compareDiceNumber(int sum);
-int getStatis(int myPoint, int status);
-void getResult();
+int getStatis(int *myPoint, int status);
+void getResult(int gameStatus);
 
 // function main begins program execution
 int main( void )
-{ 
+{
    int sum; // sum of rolled dice
-   
-
    // enumeration constants represent game status
-	enum Status { CONTINUE, WON, LOST };
+   enum Status { CONTINUE, WON, LOST };
    enum Status gameStatus; // can contain CONTINUE, WON, or LOST
 
-   // randomize random number generator using current time
-   
-
    sum = rollDice(); // first roll of the dice
-
-   int compareDiceNumber(sum);
-
-   int getStatis(int myPoint, int status);
    
-   void getResult();
-   
-
-   
+   int realStatus = compareDiceNumber(sum);
+   getResult(realStatus);
+   system("pause");
+   return 0;
 } // end main
 
 // roll dice, calculate sum and display results
@@ -54,17 +45,17 @@ int rollDice( void )
 
 
 int compareDiceNumber(int sum){
-	// enumeration constants represent game status
-	enum Status { CONTINUE, WON, LOST };
-	 enum Status gameStatus;
-	 int myPoint;
-	 switch( sum ) {
-
+    // enumeration constants represent game status
+    enum Status { CONTINUE, WON, LOST };
+    enum Status gameStatus;
+    int SecondPoint = 0;
+    
+    switch( sum ) {
+            
       // win on first roll
       case 7: // 7 is a winner
-      case 11: // 11 is a winner          
+      case 11: // 11 is a winner
          gameStatus = WON; // game has been won
-		 
          break;
 
       // lose on first roll
@@ -72,49 +63,51 @@ int compareDiceNumber(int sum){
       case 3: // 3 is a loser
       case 12: // 12 is a loser
          gameStatus = LOST; // game has been lost
-		 
          break;
 
       // remember point
-      default:                  
+      default:
          gameStatus = CONTINUE; // player should keep rolling
-         myPoint = sum; // remember the point
-         printf( "Point is %d\n", myPoint );
-		 
+         SecondPoint = sum; // remember the point
+         printf( "Point is %d\n", SecondPoint ); 
          break; // optional
    } // end switch
-		return gameStatus;
+   
+  int getStatus = getStatis(&SecondPoint, gameStatus);
    // while game not complete
+  return getStatus;
 }
- int getStatis(int myPoint, int gameStatus){
-	 int sum;
-	 enum Status { CONTINUE, WON, LOST };
-	 
-	 while ( CONTINUE == gameStatus ) { // player should keep rolling
-      sum = rollDice(); // roll dice again
 
-      // determine game status
-      if ( sum == myPoint ) { // win by making point
-         gameStatus = WON; // game over, player won
-		 return gameStatus;
-      } // end if
-      else {
-         if ( 7 == sum ) { // lose by rolling 7
-            gameStatus = LOST; // game over, player lost
-			return gameStatus;
-         } // end if
-      } // end else
+int getStatis(int *SecondPoint, int gameStatus){
+    int sum;
+    int myPoint = *SecondPoint; 
+    enum Status { CONTINUE, WON, LOST };
+
+    while ( CONTINUE == gameStatus ) { // player should keep rolling
+          sum = rollDice(); // roll dice again
+    
+          // determine game status
+          if ( sum == myPoint ) { // win by making point
+             gameStatus = WON; // game over, player won
+             return gameStatus;
+          } // end if
+          else {
+               if ( 7 == sum ) { // lose by rolling 7
+                  gameStatus = LOST; // game over, player lost
+                  return gameStatus;
+               } // end if
+          } // end else
    } // end while
 }
 
- void getResult(int gameStatus){
-	 
-	 enum Status { CONTINUE, WON, LOST };
-	// display won or lost message
-   if ( WON == gameStatus ) { // did player win?
-      puts( "Player wins" );
-   } // end if
-   else { // player lost
-      puts( "Player loses" );
-   } // end else
- }
+void getResult(int gameStatus){
+
+     enum Status { CONTINUE, WON, LOST };
+     // display won or lost message
+     if ( WON == gameStatus ) { // did player win?
+        puts( "Player wins" );
+     } // end if
+     else { // player lost
+        puts( "Player loses" );
+     } // end else
+}
