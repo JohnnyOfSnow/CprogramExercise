0d0ef8c6f8b52639e5d237b098766484cb5a0f8d
ProgramExercise征戰 <good258johnny@yahoo.com.tw>
2014-03-17 18:06:39 +0800

    Create flipOfCoin.cpp
    3月17日的作業

---
+++ flipOfCoin.cpp
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define SIZE 2
+
+int filp( void );
+
+int main(void)
+{
+	unsigned int counter = 1;
+	unsigned int coin[SIZE] = {0};
+	int result = 0;
+
+	while(counter <= 100) {
+		result = filp();
+
+		if (result == 1){
+			coin[1] = coin[1] + 1;
+		}else if (result == 0) {
+			coin[0] = coin[0] + 1;
+		}else{
+			printf("Wrong process\n");
+		}
+		counter = counter + 1;
+	}
+
+	printf("%s%17s\n", "Coin", "Frequency");
+	for(int i = 0; i < SIZE; i++){
+		printf("%4d%17d\n",i, coin[i]);
+	}
+	system("pause");
+	return 0;
+}
+
+
+int filp( void )
+{
+   int coin1; // first die
+   
+   srand( time( NULL ) );
+
+   coin1 = 1 +  ( rand() % 1); // pick random die1 value
+   
+   if (coin1 == 1) {
+		printf("It's heads\n");
+		return 1;
+   } else if(coin1 == 0) {
+		printf("It's tails\n");
+		return 0;
+   } else {
+	   printf("Wrong toss\n");
+	   return -1;
+   }
+   
+} // end function filp
