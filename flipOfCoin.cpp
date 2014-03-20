c1c3be068c67f3e07b7d8a30f91918d887bb9ad5
ProgramExercise征戰 <good258johnny@yahoo.com.tw>
2014-03-20 18:02:29 +0800

    加上註解
    為程式加上完整的註解

--- flipOfCoin.cpp
+++ flipOfCoin.cpp
@@ -1,47 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <math.h>
-#define SIZE 2
+#include <math.h> 
+#define SIZE 2 // define a constant
 
-int filp( void );
+int filp( void ); // function prototype
 
+// function main begins program execution
 int main(void)
 {
 	unsigned int counter = 1;
-	unsigned int coin[SIZE] = {0};
-	int result = 0;
-
+	unsigned int coin[SIZE] = {0}; // A array hold the result of fipping coin.
+	int result = 0; // A variable hole the he result of fipping coin.
+    
+    /**
+     *  I use while loop to simulate the fipping coin.
+     *  case 1: if the coin is head, add one to index one.
+     *  case 2: if the coin is tails, add one to index zero.
+     */
 	while(counter <= 100) {
-		result = filp();
-
+		result = filp(); // call the function int filp( void )
 		if (result == 1){
 			coin[1] = coin[1] + 1;
 		}else if (result == 0) {
 			coin[0] = coin[0] + 1;
 		}else{
-			printf("Wrong process\n");
+			printf("Wrong process\n"); // neither 1 nor 0.
 		}
 		counter = counter + 1;
 	}
 
-	printf("%s%17s\n", "Coin", "Frequency");
+	printf("%s%17s\n", "Coin", "Frequency"); // Display the title.
 	for(int i = 0; i < SIZE; i++){
-		printf("%4d%17d\n",i, coin[i]);
+		printf("%4d%17d\n",i, coin[i]); // Display the result.
 	}
-	system("pause");
-	return 0;
-}
+	system("pause"); // Let program stop, and we can see the result.
+	return 0; // Main function is sucessful.
+}// end main function
 
-
+/**
+ *  A function can fip a coin, and print the result.
+ */
 int filp( void )
 {
    int coin1; // A coin
    int coin2; // Decide the coin face
    
    coin1 = ( rand() % 2); // pick random coin value
-   coin2 = (int)(ceil(coin1));
+   coin2 = (int)(ceil(coin1)); // Get the lower integer.
    
+   /**
+    *  Use if - else if - else to distinguish the face of coin.
+    *  case 1: if the coin is head, print "It's heads".
+    *  case 2: if the coin is tails, print "It's tails".
+    */
    if (coin2 == 1) {
 		printf("It's heads\n");
 		return 1;
@@ -51,6 +63,5 @@ int filp( void )
    } else {
 	   printf("Wrong toss\n");
 	   return -1;
-   }
-   
+   }// end if 
 } // end function filp
