5aead16e84199af327c7c4eb28e622ddce5cf802
ProgramExercise征戰 <good258johnny@yahoo.com.tw>
2014-04-22 14:08:52 +0800

    完成8.7的作業
    完成程式碼即加入例外處理，加入部分注解

--- gcd.cpp
+++ gcd.cpp
@@ -10,41 +10,62 @@ int _tmain(int argc, _TCHAR* argv[])
 }*/
 
 int main(void){
-	char s[SIZE];
-	int c;
-	int x;
-	int y;
-	int i = 0;
-	
+	char s[SIZE]; // hold the string that user inputs.
+	int c; 
+	int x; // x value to be input
+	int y; // y value to be input
+	int i = 0; // for loop count
 
+    // prompt user input data
 	puts("Please enter two integer, and we can get GCD");
-
+    printf("-------------------------------------------\n");
+    
+    // user getchar to read each character
 	while(i < SIZE - 1 && (c = getchar()) != '\n'){
 		s[i++] = c;
 	}
-	s[i] = '\0';
+	s[i] = '\0'; // end the string
+	
+	sscanf(s, "%d%d", &x,&y); // Convert string into number 
+	int gcd = getGCD(x,y); // Invoke function to get gcd
+	printf("--------------------------------------------\n");
+	if (gcd == 0){
+         printf("Your input is wrong, GCD is based on non-negative number and not zero.\n");   
+    } else {
+   	     printf("We can get GCD is %d for %d and %d\n", gcd, x, y); 
+    }
 
-	sscanf(s, "%d%d", &x,&y);
-	int gcd = getGCD(x,y);
-	printf("%d", gcd);
 	system("pause");
 	return 0;
 }
 
 int getGCD(int x, int y){
-
-	int bigger = x;
-	if(bigger < y){
-		bigger = y;
-		y = x;
-	}
-
-	int count = 0;
-	for(count = y ;count <= 1; count--){
-		if(bigger % count == 0 && y % count == 0){
-			return count;
-		}
-	}
-	return 1;
+    
+    // Exception handle, x = 0 , y = 0,or x = y = 0
+    if(x * y == 0){
+         return 0;
+    }
+    
+    // Exception handle, x < 0, y < 0,or x < 0 y < 0
+    if(x < 0 || y < 0){
+         return 0;
+    }
+    
+	int bigNumber = x;
+	if(bigNumber < y){
+        bigNumber = y;
+        y = x;         
+    }
+	
+	int gcd = 0;
+	// The biggest gcd is the small number(y), so check the gcd fron it.
+	for(gcd = y; gcd >= 1; gcd--){
+            if((bigNumber % gcd == 0) && (y % gcd == 0)){
+                          return gcd; // Find the gcd
+            }
+    }
+	
+	return 1; // The small gcd is 1.
 }
+
 
